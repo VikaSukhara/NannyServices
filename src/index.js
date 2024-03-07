bde49ac690e9a50c0b59075e6bec38bf6269c133
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2024-03-07 14:51:43 +0100

    change readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from 'GlobalStyle';
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { persistor, store } from '../src/redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Toaster } from 'react-hot-toast';
+import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
